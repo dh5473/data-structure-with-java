f2f5ce677dada02cc24d526a06369ca264d38a62
Donhyeok Kang <dh5473@naver.com>
2025-02-04 19:59:04 +0900

    Feat: ArrayList implements

---
+++ src/main/java/list/ArrayList.java
@@ -0,0 +1,83 @@
+package list;
+
+import java.util.Arrays;
+
+public class ArrayList<E> implements List<E> {
+    static int DefaultSize = 10;
+    E[] data;
+    int listSize;
+
+    public ArrayList() {
+        this(DefaultSize);
+    }
+
+    public ArrayList(int size) {
+        listSize = 0;
+        data = (E[]) new Object[size];
+    }
+
+    @Override
+    public void clear() {
+        listSize = 0;
+    }
+
+    @Override
+    public void insert(int pos, E item) {
+        for (int i = listSize; i > pos; i--) {
+            data[i] = data[i - 1];
+        }
+        data[pos] = item;
+        listSize++;
+    }
+
+    @Override
+    public void append(E item) {
+        // data[listSize] = item;
+        // listSize += 1;
+        data[listSize++] = item;
+    }
+
+    @Override
+    public void update(int pos, E item) {
+        data[pos] = item;
+    }
+
+    @Override
+    public E getValue(int pos) {
+        return data[pos];
+    }
+
+    @Override
+    public E remove(int pos) {
+        E ret = data[pos];
+        listSize--;
+
+        for (int i = pos; i < listSize; i++) {
+            data[i] = data[i + 1];
+        }
+        data[listSize] = null;
+
+        return ret;
+    }
+
+    @Override
+    public int length() {
+        return listSize;
+    }
+
+    //Run | Debug
+    public static void main(String[] args){
+
+        ArrayList<Integer> mylist = new ArrayList<>();
+        mylist.append(3);
+        System.out.println(Arrays.toString(mylist.data));
+        mylist.append(5);
+        System.out.println(Arrays.toString(mylist.data));
+        mylist.insert(0, 10);
+        System.out.println(Arrays.toString(mylist.data));
+        mylist.remove(2);
+        System.out.println(Arrays.toString(mylist.data));
+
+        System.out.println(mylist.length());
+    }
+}
